f39d68049d9d151f401cdf0c2264e4a320004470
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-07-30 18:40:22 +0300

    fix catalog form

---
+++ staticfiles/catalog.js
@@ -0,0 +1,130 @@
+Vue.createApp({
+    el: '#VueAppCatalog',
+    components: {
+        VForm: VeeValidate.Form,
+        VField: VeeValidate.Field,
+        ErrorMessage: VeeValidate.ErrorMessage,
+    },
+    data() {
+        return {
+            schema1: {
+                cake: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' id';
+                }
+            },
+            schema2: {
+                name: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' имя';
+                },
+                phone: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' телефон';
+                },
+                name_format: (value) => {
+                    const regex = /^[a-zA-Zа-яА-Я]+$/
+                    if (!value) {
+                        return true;
+                    }
+                    if ( !regex.test(value)) {
+
+                        return '⚠ Формат имени нарушен';
+                    }
+                    return true;
+                },
+                email_format: (value) => {
+                    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+                    if (!value) {
+                        return true;
+                    }
+                    if ( !regex.test(value)) {
+
+                        return '⚠ Формат почты нарушен';
+                    }
+                    return true;
+                },
+                phone_format:(value) => {
+                    const regex = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+                    if (!value) {
+                        return true;
+                    }
+                    if ( !regex.test(value)) {
+
+                        return '⚠ Формат телефона нарушен';
+                    }
+                    return true;
+                },
+                email: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' почту';
+                },
+                address: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' адрес';
+                },
+                date: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' дату доставки';
+                },
+                time: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' время доставки';
+                }
+            },
+            DATA: {
+                Cake: cake_catalog.cake_titles,
+                Berries: cake_elements.berry_titles,
+                Decors: cake_elements.decor_titles,
+            },
+            Costs: {
+                Cake: cake_catalog.cake_costs,
+                Berries: cake_elements.berry_costs,
+                Decors: cake_elements.decor_costs,
+                Words: 500
+            },
+
+            Cake: 0,
+            Berries: 0,
+            Decor: 0,
+            Words: '',
+            Comments: '',
+            Designed: false,
+
+            Name: '',
+            Phone: null,
+            Email: null,
+            Address: null,
+            Dates: null,
+            Time: null,
+            DelivComments: ''
+        }
+    },
+    methods: {
+        ToStep4() {
+            this.Designed = true
+            setTimeout(() => this.$refs.ToStep4.click(), 0);
+        },
+     },
+    computed: {
+        Cost() {
+            let W = this.Words ? this.Costs.Words : 0
+            return this.Costs.Berries[this.Berries] +
+                this.Costs.Decors[this.Decor] + W + this.Costs.Cake[this.Cake]
+        }
+    }
+}).mount('#VueAppCatalog')
